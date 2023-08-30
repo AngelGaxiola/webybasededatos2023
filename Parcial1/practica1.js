39a8d4aeadfbd1d20bcfcf7adecbd11c0ea7e1eb
AngelGaxiola <124410446+AngelGaxiola@users.noreply.github.com>
2023-08-29 19:25:25 -0700

    Add files via upload
    29/08/23

---
+++ Parcial1/practica1.js
@@ -0,0 +1,20 @@
+$(document).ready(function (){
+    //alert("Funciona jquery");
+    $("#boton1").on("click", function(){
+        console.log("Funciona click boton");
+        $("#texto1").html("Texto agregado al parrafo");
+    });
+
+    $("#texto1").hover(function () {
+            $(this).css("background-color", "blue");
+            $(this).css("font-size", "50px");
+        }, function() {
+            $(this).css("background-color", "white");
+            $(this).css("font-size", "20px");
+        }
+    );
+
+    $("#boton2").click(function (e) {
+        $("#imagen1").fadeToggle("slow");
+    });
+});
